8f7d1233b1b92cfed307563e5e4c54d25a8b42ae
Angular CLI <angular-cli@angular.io>
2017-07-27 13:15:25 -0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ReggitPage } from './app.po';
+
+describe('reggit App', () => {
+  let page: ReggitPage;
+
+  beforeEach(() => {
+    page = new ReggitPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
