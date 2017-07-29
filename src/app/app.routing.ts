768577b9480cd4fd0332050af7867c4d63252f74
Renato Grael Teixeira <renato.grael@gmail.com>
2017-07-29 00:35:27 -0300

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { WelcomeComponent } from './pages/welcome';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+    { path: 'welcome', component: WelcomeComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
+
+export const routedComponents = [WelcomeComponent];
